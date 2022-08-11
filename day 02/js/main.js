72ee751fe4de80df670ca11b1519ff9bd91f117d
melinda <melinda.morales97@gmail.com>
2022-08-10 21:13:28 -0700

    more variable types stuffs ect

--- day 02/js/main.js
+++ day 02/js/main.js
@@ -1,4 +1,4 @@
-document.querySelector('h1').innerHTML = 'hello world';
+//document.querySelector('h1').innerHTML = 'hello world';
 
 
 
@@ -37,3 +37,97 @@ console.log(users[1].score);
 console.log(users[1]['score']);
 console.log(users[0]['firstname']);// access by both by bracket notation
 
+
+
+
+
+
+
+
+//strings
+
+let string1 = 'hello';
+let string2 = "world";
+let string3 = `
+
+${string1}
+
+${string2}     ${10 * 10 + 1}
+
+`;
+console.log(string3);
+
+let number1 = 0;
+let number2 = 1.27;
+let number3 = -1.27;
+
+console.log(200207 * 100_000_000 > Infinity);
+
+console.log(7 * {name: 'joe'});
+
+
+
+
+
+
+
+
+//symbol
+
+
+console.log('hello' === 'hello');
+
+let mySymbol = Symbol('mySymbol');
+let mySymbol2 = Symbol('mySymbol');
+
+console.log(mySymbol === mySymbol2);
+
+
+
+
+
+
+//type of
+
+console.log(typeof'');
+
+console.log(typeof 1);
+
+console.log(typeof true);
+
+console.log(typeof mySymbol);
+
+console.log(typeof myBigInt);
+
+
+
+
+
+
+// create an element and append it to the body
+
+const h1 = document.createElement('h1');
+
+//change the text content of the h1 element
+
+h1.innerHTML = user1.firstName;
+
+//append the h1 element to the body
+
+document.body.append(h1);
+document.body.append(ul);
+ul.append(li);
+
+// remove the h1 element from the body
+
+h1.remove();
+
+// retreive elements from the document
+
+const p = document.querySelector('p')
+
+
+// interact with the elements from the document 
+
+p.classList.add('red');
+p.style.color = 'red';
--- day 02/js/review.js
+++ day 02/js/review.js
@@ -1,1 +1,98 @@
 console.log(name)
+test = 1;
+console.log(test);
+test = 2;
+console.log(test);
+
+
+
+
+
+const test = 1;
+let test2 = 2;
+var test3 = 3;
+
+test2 = 4;
+console.log(test2);
+
+
+constantVariable = {
+    declare: true,
+    redeclare: false,
+    assign: true,
+    reassign: false,
+}
+const myconst = true;
+//myConst = false;
+//const myConst = false;
+
+
+
+letVariable = {
+    declare: true,
+    redeclare: false,
+    assign: true,
+    reassign: true,
+}
+let myLet = true;
+myLet = false;
+//let myLet = false;
+
+
+varVariable = {
+    declare: true,
+    redeclare: true,
+    assign: true,
+    reassign: true,
+}
+var mayVar = true;
+myVar = false;
+var myVar = false;
+
+
+
+
+whatIsThis = true
+
+//operators
+//arithmetic Operators
+// + - * / %
+
+console.log('Operators')
+console.log(1 + 1);
+console.log(10 - 1);
+console.log(10 * 2);
+console.log(10 / 2);
+console.log(7 % 2);
+
+
+//assignment Operators
+
+// = += -= /= %=
+console.log('assignment Operators')
+let x = 10;
+console.log(x); //10
+console.log(2 + x); //12
+console.log(x); //10
+x += 2; //12
+x += 2; //14
+console.log(x); //14
+x /= 2;
+
+console.log(x);
+
+
+
+
+
+
+//Comparison Operators
+// == != === !== > < >= <=
+// always return boolean
+
+
+console.log(20 == '20'); //true
+console.log(20 != 20); //false
+console.log(20 === 20); //true
+console.log(20 !=== 20); //false
+console.log(20 === '20'); //false
