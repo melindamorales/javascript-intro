87bf85fe04b94cf9c83542f885f7c04eaa31f7bb
melinda <melinda.morales97@gmail.com>
2022-08-29 23:38:38 -0700

    did day07 practice

--- day 07/practice.js
+++ day 07/practice.js
@@ -1,11 +1,19 @@
-
 const h1 = document.createElement('h1');
-h1.innerHTML = 'hoiiiii';
-document.body.append(h1);
+h1.innerText = 'albert is da best';
+document.body.appendChild(h1);
 h1.addEventListener('click', () => {
     h1.style.color = 'pink';
-});
+    document.body.style.color = 'white';
+    document.body.style.backgroundColor = 'blue';
+})
+
+const h2 = document.createElement('h2');
+h2.innerText = 'albert meows';
+document.body.appendChild(h2);
+h1.addEventListener('click', () => {
+    h2.style.color = 'purple';
+})
 
 
-
+h2.removeEventListener('click',);
 
